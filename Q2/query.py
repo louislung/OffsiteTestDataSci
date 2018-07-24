18a3a61b4b345b607de415c185b9812e06f99f6b
Kit Lung Law <llawhk@hk1.ibm.com>
2018-07-24 08:54:48 +0800

    Q2 solution ready

--- Q2/query.py
+++ Q2/query.py
@@ -6,6 +6,9 @@
 ##################
 import os
 from pathlib import Path
+import time
+import io
+import re
 
 
 ####################
@@ -18,27 +21,29 @@ date_from = '2017-08-24'
 time_from = '00:00:00'
 date_to = '2017-08-25'
 time_to = '23:59:59'
+suffix = '.jpg'
+suffix_regex = re.compile(re.escape(suffix) + '$')
 
 
 ################
 # Main Program #
 ################
-def get_data(input_filename, delimiter = b'\t'):
-    with open(input_filename, 'r+b') as f:
-        for record in f:                 # traverse sequentially through the file
-            x = record.split(delimiter)  # parsing logic goes here (binary, text, JSON, markup, etc)
-            yield x                      # emit a stream of things
-            #  (e.g., words in the line of a text file,
-            #   or fields in the row of a CSV file)
-
+def get_data(input_filename, delimiter = '\t'):
+    with io.open(input_filename, mode = 'r+', encoding = 'utf-8') as f:
+        for record in f:
+            x = record.rstrip().split(delimiter)
+            yield x
 
 
 if __name__ == '__main__':
-    print('main')
     log_generator = get_data(data_dir / log_file)
-    next(log_generator) #skip header
+    header = next(log_generator) #skip header
+    size = 0
     for row in log_generator:
-        print(row)
-        if str(row[0] == '2017­08­24':
-            print('2017­08­24')
-    print('end')
+        timestamp = time.strptime((row[header.index('# date')] + ' ' + row[header.index('time')]),'%Y-%m-%d %H:%M:%S')
+        if timestamp >= time.strptime(date_from + ' ' + time_from,'%Y-%m-%d %H:%M:%S') and timestamp <= time.strptime(date_to + ' ' + time_to,'%Y-%m-%d %H:%M:%S'):
+            if suffix_regex.search(row[header.index('url')]):
+                size += float(row[header.index('size')])
+
+    print('Answers:')
+    print('Total size of file with suffix ({0}) = {1}'.format(suffix,size))
